2da8c88408db595eb6c5d76e2673c9845ac6498a
Hendra Permana <hynra@yahoo.com>
2024-01-10 15:36:46 +0700

    feat: pertemuan 10

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,16 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonInput, IonItem, IonList } from '@ionic/react';
+import { IonButton } from '@ionic/react';
 import './Tab1.css';
+import React from 'react';
+import { IonAlert } from '@ionic/react';
 
 const Tab1: React.FC = () => {
+
+  const [userName, setUsername] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const [isOpen, setIsOpen] = React.useState(false);
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +24,40 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <div className='container'>
+          <img src='/favicon.png' width='80px' />
+          <IonInput
+            labelPlacement="stacked"
+            value={userName}
+            onIonInput={(event) => setUsername(event.target.value.toString())}
+            label="Username: "
+            type='email'
+          >
+
+          </IonInput>
+          <IonInput
+            labelPlacement="stacked"
+            label="Password: "
+            type='password'
+            value={password}
+            onIonInput={(event) => setPassword(event.target.value.toString())}
+          >
+
+          </IonInput>
+          <IonButton
+            expand="block"
+            onClick={() => setIsOpen(true)}
+          >
+            Login
+          </IonButton>
+          <IonAlert
+            isOpen={isOpen}
+            header="Login"
+            message={`Username: ${userName}, Password: ${password}`}
+            buttons={['OK']}
+            onDidDismiss={() => setIsOpen(false)}
+          />
+        </div>
       </IonContent>
     </IonPage>
   );
